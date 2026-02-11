ed8da7bb034906d39cb78df6d1ee9f6d59e44365
rtwshrpk <ratthawit01zx@gmail.com>
2026-02-11 15:00:06 +0700

    Refactor #2 - add paceCal function

--- common.js
+++ common.js
@@ -9,6 +9,17 @@ function validatePace(pace) {
 		}
 }
 
+
+function paceCal(pace,dist) {
+		var min = dist * pace;
+		var hour = 0;
+		if(min > 60){
+			hour = Math.floor(min / 60);          
+			min = min % 60;
+		}
+		return {"min":min,"hour":hour};
+}
+
 function calculate(p,d){
 		var msg = "";
 		var pace = parseFloat(p);
@@ -17,12 +28,7 @@ function calculate(p,d){
 		var chk = validatePace(pace);
 		if (chk != null) return chk;
 	//---------------------------
-		var min = dist * pace;
-		var hour = 0;
-		if(min > 60){
-			hour = Math.floor(min / 60);          
-			min = min % 60;
-		}
+		var pc = paceCal(pace,dist)
 	//---------------------------
 		msg = "successfully calculated!"
 		return {"status":1,"message":msg,"hour":hour,"min":min};
